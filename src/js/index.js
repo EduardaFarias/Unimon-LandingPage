c207deabc9590bdba071acd0cadd9a3a8a20ad44
= <=>
2023-10-02 14:07:05 -0300

    Adicionando FavIcon

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+
+function alternarModal() {
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () =>{
+    alternarModal();
+    video.setAttribute("src", "https://www.youtube.com/embed/Cb4WV4aXBpk");
+} );
+
+botaoFecharModal.addEventListener("click", () =>{
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+ 
+
+ 
+
+
